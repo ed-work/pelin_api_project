e70c296e26614966c091a9e405005e01cf0d564c
dayatz <dayat.py@gmai.com>
2015-08-20 13:35:22 +0800

    apps model

--- pelin_api/apps/group/models.py
+++ pelin_api/apps/group/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django_extensions.db.models import TitleDescriptionModel
+from apps.core.models import User, Student, Teacher
 
-# Create your models here.
+
+def generate_filename(self, filename):
+    """
+    generate destination FileField generate_filename arg to the following pattern:
+    MEDIA_ROOT/<group_name>_<group_id>/filename
+    """
+    return "%s_%s/%s" % (self.title, self.pk, filename)
+
+
+class TimeStamped(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class GroupFileModel(models.Model):
+    group = models.ForeignKey('Group')
+    file = models.FileField(upload_to=generate_filename)
+
+    class Meta:
+        abstract = True
+
+
+class Group(TimeStamped, TitleDescriptionModel):
+    teacher = models.ForeignKey(Teacher)
+    members = models.ManyToManyField(Student)
+
+
+class PendingApproval(TimeStamped):
+    group = models.ForeignKey(Group)
+    student = models.ForeignKey(Student)
+
+
+class Discussion(TimeStamped, GroupFileModel):
+    user = models.ForeignKey(User)
+    text = models.TextField()
+    file = models.FileField(upload_to=generate_filename, blank=True, null=True)
+
+
+class Lesson(TimeStamped, TitleDescriptionModel, GroupFileModel):
+    pass
+
+
+class Assignment(TimeStamped, TitleDescriptionModel, GroupFileModel):
+    due_date = models.DateTimeField()
--- pelin_api/pelin_api/settings.py
+++ pelin_api/pelin_api/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_extensions',
+
+    # project apps
+    'apps.core'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +105,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
