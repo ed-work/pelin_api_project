5549b884aeb885b349bd7c847586dcd796c3d2b9
dayatz <dayat.py@gmail.com>
2015-08-30 10:07:49 +0800

    show group url

--- pelin_api/apps/group/serializers.py
+++ pelin_api/apps/group/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+
 from . import models as group_models
 
 
 class GroupSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField()
+
+    def get_url(self, obj):
+        return reverse('api:group-detail', kwargs={'pk': obj.pk},
+                       request=self.context.get('request'))
+
     class Meta:
         model = group_models.Group
         extra_kwargs = {
-            'members': { 'read_only': True, 'required': False },
-            'teacher': { 'required': False }
+            'members': {'read_only': True, 'required': False},
+            'teacher': {'required': False}
         }
