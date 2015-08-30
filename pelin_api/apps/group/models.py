59562aac1d52df964f8766fe06ae9bf38be5f2b2
dayatz <dayat.py@gmail.com>
2015-08-30 11:54:50 +0800

    change and add unicode to group models

--- pelin_api/apps/group/models.py
+++ pelin_api/apps/group/models.py
@@ -31,10 +31,17 @@ class Group(TimeStamped, TitleDescriptionModel):
     teacher = models.ForeignKey(User, related_name='group_teacher')
     members = models.ManyToManyField(User, related_name='group_members')
 
+    def __unicode__(self):
+        return "%s (%s %s)" % (
+        self.title, self.teacher.first_name, self.teacher.last_name)
+
 
 class PendingApproval(TimeStamped):
     group = models.ForeignKey(Group)
-    student = models.ForeignKey(Student)
+    student = models.ForeignKey(User, related_name='pending_approval')
+
+    def __unicode__(self):
+        return "%s: %s" % (self.group.title, self.student.first_name)
 
 
 class Discussion(TimeStamped):
@@ -43,9 +50,15 @@ class Discussion(TimeStamped):
     text = models.TextField()
     file = models.FileField(upload_to=generate_filename, blank=True, null=True)
 
+    def __unicode__(self):
+        return "%s: %s" % (self.user.first_name, self.group.title)
+
 
 class Lesson(TimeStamped, TitleDescriptionModel):
     group = models.ForeignKey(Group)
+
+    def __unicode__(self):
+        return "%s: %s" % (self.title, self.group.title)
 
 
 class LessonFiles(models.Model):
@@ -56,9 +69,15 @@ class LessonFiles(models.Model):
 class Assignment(TimeStamped, TitleDescriptionModel, GroupFileModel):
     due_date = models.DateTimeField()
 
+    def __unicode__(self):
+        return "%s %s" % (self.title, self.due_date)
+
 
 class StudentAssignment(TimeStamped):
     assignment = models.OneToOneField(Assignment)
-    student = models.OneToOneField(Student)
+    student = models.OneToOneField(User)
     text = models.TextField()
     file = models.FileField(upload_to=generate_filename)
+
+    def __unicode__(self):
+        return "%s: %s" % (self.student.first_name, self.assignment.title)
