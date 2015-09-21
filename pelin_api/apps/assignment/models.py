716ee3a10b6243021195334a12bdc1d76db12eef
dayatz <dayat.py@gmail.com>
2015-09-21 09:40:24 +0800

    move some models to each app

--- pelin_api/apps/assignment/models.py
+++ pelin_api/apps/assignment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from apps.core.models import TimeStamped, User, generate_filename
+from django_extensions.db.models import TitleDescriptionModel
+from apps.group.models import GroupFileModel
 
-# Create your models here.
+
+class Assignment(TimeStamped, TitleDescriptionModel, GroupFileModel):
+    due_date = models.DateTimeField()
+
+    def __unicode__(self):
+        return "%s %s" % (self.title, self.due_date)
+
+
+class StudentAssignment(TimeStamped):
+    assignment = models.OneToOneField(Assignment)
+    student = models.OneToOneField(User)
+    text = models.TextField()
+    file = models.FileField(upload_to=generate_filename)
+
+    def __unicode__(self):
+        return "%s: %s" % (self.student.first_name, self.assignment.title)
--- pelin_api/apps/post/models.py
+++ pelin_api/apps/post/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from apps.core.models import TimeStamped, User, generate_filename
+from apps.group.models import Group
 
-# Create your models here.
+
+class Post(TimeStamped):
+    group = models.ForeignKey(Group)
+    user = models.ForeignKey(User)
+    text = models.TextField()
+    file = models.FileField(upload_to=generate_filename, blank=True, null=True)
+
+    def __unicode__(self):
+        return "%s: %s" % (self.user.first_name, self.group.title)
