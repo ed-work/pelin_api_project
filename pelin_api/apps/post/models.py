29c5b91fe56ca8866cb3d8f6bc06a2531af1d248
dayatz <dayat.py@gmail.com>
2015-09-22 18:49:57 +0800

    fix uploaded filename

--- pelin_api/apps/post/models.py
+++ pelin_api/apps/post/models.py
@@ -1,6 +1,16 @@
+import urllib2
 from django.db import models
-from apps.core.models import TimeStamped, User, generate_filename
+from apps.core.models import TimeStamped, User
 from apps.group.models import Group
+
+
+def generate_filename(self, filename):
+    """
+    generate destination FileField filename arg to the following pattern:
+    MEDIA_ROOT/<group_id>_<group_name>/filename
+    """
+    filename = urllib2.unquote(filename)
+    return "%s_%s/%s" % (self.group.pk, self.group.title, filename)
 
 
 class Post(TimeStamped):
