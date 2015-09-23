dd9f9474d565bb7b3dab1df5d37787f281e85ce9
dayatz <dayat.py@gmail.com>
2015-09-23 14:14:10 +0800

    include group_url to lesson serializer

--- pelin_api/apps/lesson/serializers.py
+++ pelin_api/apps/lesson/serializers.py
@@ -1,24 +1,37 @@
+from os.path import basename
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+from apps.group.serializers import GroupSerializer
 from .models import Lesson, LessonFiles
 
 
 class LessonFilesSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+
+    def get_name(self, obj):
+        return basename(obj.file.name)
+
     class Meta:
         model = LessonFiles
-        fields = ('id', 'file')
+        fields = ('id', 'name', 'file')
         extra_kwargs = {
             'lesson': {'required': False}
         }
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    group_url = serializers.SerializerMethodField()
     files = LessonFilesSerializer(required=False, read_only=True, many=True)
 
     class Meta:
         model = Lesson
         extra_kwargs = {
-            'group': {'required': False}
+            'group': {'required': False, 'write_only': True}
         }
+
+    def get_group_url(self, obj):
+        return reverse('api:group-detail', kwargs={'pk': obj.group.pk},
+                       request=self.context.get('request'))
 
     def create(self, validated_data):
         lesson = super(LessonSerializer, self).create(validated_data)
--- pelin_api/apps/lesson/views.py
+++ pelin_api/apps/lesson/views.py
@@ -1,14 +1,18 @@
 from rest_framework import viewsets
-from rest_framework.parsers import FileUploadParser
 
 from apps.core.views import BaseLoginRequired
 from apps.group.models import Group
+from apps.group.permissions import IsMemberOrTeacher
+from .permissions import LessonPermission
 from .serializers import LessonSerializer
 from .models import Lesson
 
 
 class LessonViewSet(BaseLoginRequired, viewsets.ModelViewSet):
     serializer_class = LessonSerializer
+
+    # def get_permissions(self):
+    #     self.permission_classes += (IsMemberOrTeacher, LessonPermission)
 
     def get_queryset(self):
         return Lesson.objects.filter(group__pk=self.kwargs.get('group_pk'))
