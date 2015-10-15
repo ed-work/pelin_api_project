90b017af6ceae8224cc97bca52e62e63cbc52a10
dayatz <dayat.py@gmail.com>
2015-10-15 09:53:43 +0800

    create message model

--- pelin_api/apps/message/models.py
+++ pelin_api/apps/message/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from apps.core.models import User
 
-# Create your models here.
+
+STATUS_CHOICES = (
+    ('s', 'Send'),
+    ('r', 'Read'),
+)
+
+class Conversation(models.Model):
+    user_1 = models.ForeignKey(User)
+    user_2 = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation)
+    user = models.ForeignKey(User)
+    text = models.TextField()
+    sent = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='s')
