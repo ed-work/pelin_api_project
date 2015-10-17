d9a9602ed50789dc189b02e865d43b9e45049421
dayatz <dayat.py@gmail.com>
2015-10-17 10:53:57 +0800

    serialize paginated conversation

--- pelin_api/apps/message/views.py
+++ pelin_api/apps/message/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 from apps.core.models import User
 from apps.core.views import BaseLoginRequired
@@ -14,24 +15,32 @@ class ConversationViewSet(BaseLoginRequired, viewsets.ModelViewSet):
         return Conversation.objects.filter(
             Q(user_1=self.request.user) | Q(user_2=self.request.user))
 
-    def retrieve(self, request, *args, **kwargs):
+    def get_conversation(self):
         try:
             conversation = self.get_object()
-        except:
+        except Conversation.DoesNotExist:
             try:
-                target_user = User.objects.get_with(kwargs.get('pk'))
+                target_user = User.objects.get_with(self.kwargs.get('pk'))
                 conversation =  Conversation.objects.get(
-                    (Q(user_1=request.user) & Q(user_2=target_user)) |
-                    (Q(user_1=target_user) & Q(user_2=request.user))
+                    (Q(user_1=self.request.user) & Q(user_2=target_user)) |
+                    (Q(user_1=target_user) & Q(user_2=self.request.user))
                 )
-            except:
+            except Conversation.DoesNotExist:
                 conversation = None
+
+        return conversation
+
+    def retrieve(self, request, *args, **kwargs):
+        conversation = self.get_conversation()
 
         if conversation:
             messages = Message.objects.filter(conversation=conversation)
             messages_page = self.paginate_queryset(messages)
             serializer = MessageSerializer(messages_page, many=True)
-            return Response(serializer.data)
+            return self.get_paginated_response(serializer.data)
         else:
             return super(ConversationViewSet, self).retrieve(request, *args, **kwargs)
 
+    @detail_route(methods=['POST'])
+    def reply(self, request, pk):
+        conversation = self.get_conversation()
