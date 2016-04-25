46fda0759836e2af24ff51f6c8089e01a50bc38a
dayatz <dayat.py@gmail.com>
2016-04-25 20:19:01 +0800

    update openshift config

--- pelin_api/config/settings/openshift.py
+++ pelin_api/config/settings/openshift.py
@@ -3,6 +3,8 @@ from .base import *
 from .apps import INSTALLED_APPS
 
 DEBUG = False
+INSTALLED_APPS = INSTALLED_APPS
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
@@ -16,3 +18,5 @@ DATABASES = {
         'PORT': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT')
     }
 }
+
+STATIC_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'static')
