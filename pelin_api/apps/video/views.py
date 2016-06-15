6a8296ba97c358b7c90d04aff6c1135a61a9e8b2
dayatz <dayat.py@gmail.com>
2016-06-15 21:24:30 +0800

    search page

--- pelin_api/apps/video/views.py
+++ pelin_api/apps/video/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
@@ -11,7 +12,7 @@ class VideoViewset(ModelViewSet):
     serializer_class = VideoSerializer
     psermission_classes = (VideoPermission,)
     authentication_classes = (JSONWebTokenAuthentication,)
-    queryset = Video.objects.all()
+    queryset = Video.objects.select_related('user')
     filter_fields = ['category']
 
     def get_permissions(self):
@@ -24,10 +25,40 @@ class VideoViewset(ModelViewSet):
 
     def get_queryset(self):
         if 'mine' in self.request.query_params:
-            return Video.objects.filter(user=self.request.user)
+            return self.queryset.filter(user=self.request.user)
         return super(VideoViewset, self).get_queryset()
 
 
 def index(request):
-    videos = Video.objects.all()
+    videos = Video.objects.select_related('user')
     return render(request, 'index.html', {'videos': videos})
+
+
+def search(request):
+    try:
+        keyword = request.GET.get('q')
+        videos = Video.objects.filter(
+            Q(title__icontains=keyword) |
+            Q(user__name__icontains=keyword) |
+            Q(category__name__in=[keyword])).distinct()
+    except:
+        keyword = ''
+        videos = None
+    return render(request, 'search.html', {
+        'keyword': keyword,
+        'videos': videos
+    })
+
+
+def kategori(request, category):
+    videos = Video.objects.filter(category__name__in=[category])
+    return render(request, 'kategori.html', {'videos': videos})
+
+
+def video_detail(request, pk):
+    try:
+        video = Video.objects.get(pk=pk)
+    except Video.DoesNotFound:
+        video = None
+
+    return render(request, 'video_detail.html', {'video': video})
