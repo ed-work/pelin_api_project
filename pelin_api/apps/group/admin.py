ac419b2a3a284346e9d22ef095db95877c2b20a2
dayatz <dayat.py@gmail.com>
2016-06-21 15:23:10 +0800

    custom action to approve all

--- pelin_api/apps/group/admin.py
+++ pelin_api/apps/group/admin.py
@@ -5,10 +5,35 @@ from .models import Group, PendingApproval
 class GroupAdmin(admin.ModelAdmin):
     list_display = ['title', 'get_teacher', 'teacher']
     search_fields = ['title', 'teacher__name']
+    ordering = ['title']
 
     def get_teacher(self, obj):
         return obj.teacher.name
 
+
+def approve_all(modeladmin, request, queryset):
+    for pending in queryset:
+        if pending.user not in pending.group.members.all():
+            pending.group.members.add(pending.user)
+    queryset.delete()
+approve_all.short_description = 'Approve selected'
+
+
+class PendingApprovalAdmin(admin.ModelAdmin):
+    list_display = ['group', 'name', 'nim', 'created_at']
+    search_fields = ['user__name', 'user__student__nim', 'group__title']
+    ordering = ['group__title']
+    actions = [approve_all]
+
+    def name(self, obj):
+        return obj.user.name
+
+    def nim(self, obj):
+        return obj.user.student.nim
+
+    def group(self, obj):
+        return obj.group.title
+
+
 admin.site.register(Group, GroupAdmin)
-# TODO: custom action
-admin.site.register(PendingApproval)
+admin.site.register(PendingApproval, PendingApprovalAdmin)
