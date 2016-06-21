546e8fbd303e086ecbebb65b9e7a0edfde48b36f
dayatz <dayat.py@gmail.com>
2016-06-21 22:05:07 +0800

    custom user form admin

--- pelin_api/apps/core/admin.py
+++ pelin_api/apps/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User as CustomUser, Teacher, Student
+from .forms import UserCreationForm, UserChangeForm
 
 
 class StudentAdmin(admin.ModelAdmin):
@@ -18,11 +20,35 @@ class TeacherAdmin(admin.ModelAdmin):
         return obj.user.name
 
 
-class UserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     list_display = ['name', 'email', 'status']
     search_fields = ['name', 'email']
+    form = UserChangeForm
+    add_form = UserCreationForm
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('name', 'phone')}),
+        ('Permissions', {'fields': ('is_superuser', 'is_admin',
+                                    'is_staff', 'is_active',
+                                    'user_permissions')})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }
+        ),
+        ('Personal Info', {'fields': ('name', 'phone')}),
+        ('Permissions', {'fields': ('is_superuser', 'is_admin',
+                                    'is_staff', 'is_active',
+                                    'user_permissions')})
+    )
+
+    def get_queryset(self, request):
+        qs = super(CustomUserAdmin, self).get_queryset(request)
+        return qs.exclude(is_superuser=True)
 
 
-admin.site.register(CustomUser, UserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Teacher, TeacherAdmin)
 admin.site.register(Student, StudentAdmin)
